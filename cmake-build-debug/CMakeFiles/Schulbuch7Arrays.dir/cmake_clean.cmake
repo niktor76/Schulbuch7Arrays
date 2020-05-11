655e44ed97fc9b278e55306871fe7a8baeeaf43d
Viktor Steck <v.steck@bitbricks.de>
2020-05-11 14:03:22 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Schulbuch7Arrays.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Schulbuch7Arrays.dir/main.cpp.o"
+  "Schulbuch7Arrays"
+  "Schulbuch7Arrays.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Schulbuch7Arrays.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
